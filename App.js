ddca5c623580ac2bbaf58396bb02286f1cf60378
paultebeila <paultebeila@gmail.com>
2022-09-13 15:56:02 +0200

    Pictures added

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+
+
+const image = { uri: "./images/senepe.jpn" };
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.card}>Birthday Card</Text>
+      <Image 
+      style={styles.img}
+      source={require('./images/senepe2.gif')}
+      />
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
       <View style={styles.body}>
         <Text>
           Happy birthday We wish 
@@ -13,6 +21,7 @@ export default function App() {
           and never let anyone ruin it for you.
           </Text>
       </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,16 +30,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
     backgroundColor: 'magenta',
     alignItems: 'center',
     justifyContent: 'center',
+  },image: {
+    flex: 1,
+    justifyContent: "center",
   },
   card: {
     fontSize: 30,
     fontWeight: 'bold',
   },
   body: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: -200,
     marginLeft: '15%',
     marginRight: '15%',
+  },
+  img: {
+    marginTop: 100,
+    height: 200,
+    width: 200,
   }
 });
